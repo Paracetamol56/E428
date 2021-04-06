1f2c1a22a75523c70a7e751f5973db9014d9e626
Jame_FLC <jame.flochlecarour@gmail.com>
2021-04-06 12:30:44 +0200

    Added Unity Project
    Added Unity project with URP rendering and Input System package

---
+++ Projet/E428/Assets/Scripts/Input/PlayerInput.cs
@@ -0,0 +1,225 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Input/PlayerInput.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerInput : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerInput()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerInput"",
+    ""maps"": [
+        {
+            ""name"": ""Gameplay"",
+            ""id"": ""5f9b77df-634b-47e1-bb02-f20f2413818d"",
+            ""actions"": [
+                {
+                    ""name"": ""Button"",
+                    ""type"": ""Button"",
+                    ""id"": ""a8b2c486-573f-4051-b1b6-33d474882827"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Horizontal_Axis"",
+                    ""type"": ""Value"",
+                    ""id"": ""c4ab8a0d-aa32-45a6-bb28-7bce4d4dbafc"",
+                    ""expectedControlType"": ""Axis"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""c90d23f0-0ff7-4e38-b87e-50488aca4022"",
+                    ""path"": """",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Default_Control"",
+                    ""action"": ""Button"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""Keyboard"",
+                    ""id"": ""c44ff318-b034-4924-a0db-c58ccfc84146"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Horizontal_Axis"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""d0213472-6057-4244-b9dc-714b0e9693e8"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Default_Control"",
+                    ""action"": ""Horizontal_Axis"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""8556830c-ac64-41a8-a231-8e55e7b3c0be"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Default_Control"",
+                    ""action"": ""Horizontal_Axis"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""094a4cc8-227e-419e-b5d2-5d9cbbbbbe14"",
+                    ""path"": ""<Gamepad>/leftStick/x"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Default_Control"",
+                    ""action"": ""Horizontal_Axis"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Default_Control"",
+            ""bindingGroup"": ""Default_Control"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Gamepad>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                },
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                },
+                {
+                    ""devicePath"": ""<Mouse>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+        // Gameplay
+        m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
+        m_Gameplay_Button = m_Gameplay.FindAction("Button", throwIfNotFound: true);
+        m_Gameplay_Horizontal_Axis = m_Gameplay.FindAction("Horizontal_Axis", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Gameplay
+    private readonly InputActionMap m_Gameplay;
+    private IGameplayActions m_GameplayActionsCallbackInterface;
+    private readonly InputAction m_Gameplay_Button;
+    private readonly InputAction m_Gameplay_Horizontal_Axis;
+    public struct GameplayActions
+    {
+        private @PlayerInput m_Wrapper;
+        public GameplayActions(@PlayerInput wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Button => m_Wrapper.m_Gameplay_Button;
+        public InputAction @Horizontal_Axis => m_Wrapper.m_Gameplay_Horizontal_Axis;
+        public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GameplayActions set) { return set.Get(); }
+        public void SetCallbacks(IGameplayActions instance)
+        {
+            if (m_Wrapper.m_GameplayActionsCallbackInterface != null)
+            {
+                @Button.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnButton;
+                @Button.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnButton;
+                @Button.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnButton;
+                @Horizontal_Axis.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnHorizontal_Axis;
+                @Horizontal_Axis.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnHorizontal_Axis;
+                @Horizontal_Axis.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnHorizontal_Axis;
+            }
+            m_Wrapper.m_GameplayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Button.started += instance.OnButton;
+                @Button.performed += instance.OnButton;
+                @Button.canceled += instance.OnButton;
+                @Horizontal_Axis.started += instance.OnHorizontal_Axis;
+                @Horizontal_Axis.performed += instance.OnHorizontal_Axis;
+                @Horizontal_Axis.canceled += instance.OnHorizontal_Axis;
+            }
+        }
+    }
+    public GameplayActions @Gameplay => new GameplayActions(this);
+    private int m_Default_ControlSchemeIndex = -1;
+    public InputControlScheme Default_ControlScheme
+    {
+        get
+        {
+            if (m_Default_ControlSchemeIndex == -1) m_Default_ControlSchemeIndex = asset.FindControlSchemeIndex("Default_Control");
+            return asset.controlSchemes[m_Default_ControlSchemeIndex];
+        }
+    }
+    public interface IGameplayActions
+    {
+        void OnButton(InputAction.CallbackContext context);
+        void OnHorizontal_Axis(InputAction.CallbackContext context);
+    }
+}
